6651b3bc1f3ba3f0eaac74072b6042f6ebaa3944
robert <robertvrenozi@thedancingbits.com>
2017-09-25 16:16:25 +0200

    better control flow...

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'url': 'mongodb://localhost/helis_webrtc'
-    // 'url': 'mongodb://rob_:test123@ds147964.mlab.com:47964/helis_webrtc'
+    /*'url': 'mongodb://localhost/helis_webrtc'*/
+    'url': 'mongodb://rob_:test123@ds147964.mlab.com:47964/helis_webrtc'
 }
